3892e1190ffb42931064eb30811f05887f489f6b
Mike Vollmer <mike@recurial.com>
2015-01-28 00:10:30 -0500

    Added Search.hs

---
+++ Control/Monad/Tune/Search.hs
@@ -0,0 +1,17 @@
+
+{- |
+Copyright    : 2015 Mike Vollmer
+License      : BSD2
+
+Search strategies that cooperate with TunerT.
+-}
+
+module Control.Monad.Tune.Search (
+  crossover,
+  mutate
+  ) where
+
+import Control.Monad.Tune
+
+crossover = undefined
+mutate = undefined
